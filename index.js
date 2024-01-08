1a8ef3a155955d82ab63a9ad6a6af1754f3be87d
Lokeshrawat1 <114060111+Lokeshrawat1@users.noreply.github.com>
2024-01-08 17:25:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const heading = document.getElementById('heading');
+
+document.querySelector(".icon").addEventListener('click' , () => {
+    document.body.style.backgroundColor = 'gray';
+   
+
+})
